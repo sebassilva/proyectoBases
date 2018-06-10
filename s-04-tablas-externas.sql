8c4ddec62b7b5bc647bb63fef7debf931ccb7f37
Gerardo LSJ <gerardolsj@gmail.com>
2018-06-09 22:22:28 -0500

    dates

--- s-04-tablas-externas.sql
+++ s-04-tablas-externas.sql
@@ -1,30 +1,6 @@
 CREATE DIRECTORY ext_tab_dir AS '/home/oracle/app';
 GRANT READ ON DIRECTORY ext_tab_dir TO prueba1;
 
-
-CREATE TABLE emp_load
-      (employee_number      CHAR(5),
-       employee_dob         CHAR(20),
-       employee_last_name   CHAR(20),
-       employee_first_name  CHAR(15),
-       employee_middle_name CHAR(15),
-       employee_hire_date   DATE)
-    ORGANIZATION EXTERNAL
-      (TYPE ORACLE_LOADER
-      DEFAULT DIRECTORY ext_tab_dir
-      ACCESS PARAMETERS
-        (RECORDS DELIMITED BY NEWLINE
-         FIELDS (employee_number      CHAR(2),
-                 employee_dob         CHAR(21),
-                 employee_last_name   CHAR(19),
-                 employee_first_name  CHAR(12),
-                 employee_middle_name CHAR(12),
-                 employee_hire_date   CHAR(11) date_format DATE mask "mm/dd/yyyy"
-                )
-        )
-      LOCATION ('info.dat')
-     );
-     
 CREATE TABLE ext_tab (
 empno  CHAR(4),
 ename  CHAR(20),
--- s-09-carga-inicial.sql
+++ s-09-carga-inicial.sql
@@ -1,11 +1,26 @@
 -- autores --
 
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Nunc Company','turpis.Nulla.aliquet@Suspendisse.net','Rashad Gonzales','Geoffrey','September','Mauris Incorporated');
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Aliquam Eu Accumsan Inc.','dolor.dapibus.gravida@tristiquepharetraQuisque.org','Brent Obrien','Omar','Maya','Eget Ipsum Suspendisse Corporation');
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Semper Corp.','a.odio@vestibulumneceuismod.co.uk','Howard Knowles','Reuben','Denise','Vivamus Rhoncus Donec Company');
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Vivamus PC','luctus.et@miDuis.org','Brian Head','Ulysses','Abra','Et Magnis Dis Industries');
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Diam Limited','dapibus.id@scelerisquemollisPhasellus.edu','Hyatt Ayala','Zephania','Scarlett','Nam Consequat PC');
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Laoreet Libero Industries','Quisque.porttitor.eros@purusaccumsaninterdum.ca','Austin Tillman','Hiram','Madison','Mauris Ut Quam Associates');
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Fusce Fermentum Ltd','nisl@SuspendissesagittisNullam.edu','Bevis Joseph','Mason','Karyn','Egestas Associates');
-INSERT INTO `AUTOR` (`EMPRESA`,`EMAIL`,`NOMBRE`,`AP_PATERNO`,`AP_MATERNO`,`OCUPACION`) VALUES ('Nec Leo Morbi Limited','a.facilisis.non@ipsum.co.uk','Solomon Whitney','Orlando','Mariam','Nec Tempus Consulting');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Vehicula Pellentesque Corporation','a@ultriciesligula.com','Samuel','Kasper','Britanney','973858 9721');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Cras LLC','penatibus.et.magnis@idrisusquis.co.uk','Maryam','Kasimir','Cyrus','017867 6623');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Ut Industries','tempor.arcu.Vestibulum@Donec.org','Macy','Branden','Hedda','565221 1607');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Dolor Donec Fringilla Limited','Aliquam@Mauris.com','Zena','Cole','Mari','646260 1136');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Sed Libero Proin Inc.','nibh.enim.gravida@parturientmontesnascetur.ca','Alyssa','Macaulay','Kasper','339817 8941');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Donec Feugiat Institute','Morbi@magnaaneque.edu','Demetrius','Linus','Barrett','076138 6630');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Integer Mollis PC','sit.amet@tellusjusto.edu','Freya','Chase','Yvette','893271 9746');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Non Lobortis Foundation','Sed.et.libero@nasceturridiculusmus.com','Dai','Fulton','Slade','168322 5344');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Ante Ipsum Foundation','luctus@nislMaecenas.ca','Hannah','Mark','Macon','798278 0012');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Sit Amet LLP','nec.eleifend.non@non.co.uk','Solomon','Hamilton','Julie','595550 8832');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Donec Dignissim Company','a@gravidasit.org','Ann','Ezekiel','Bradley','747084 4668');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Sed LLC','ultrices.Vivamus.rhoncus@Nullasempertellus.co.uk','Yuli','Raymond','Jordan','794515 7977');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Facilisis Facilisis Magna Company','vel@Cumsociis.co.uk','Lane','Beck','Lunea','728621 7984');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Morbi Vehicula Incorporated','sed.dolor.Fusce@orciluctus.com','Laith','Tobias','Fredericka','306578 1852');
+INSERT INTO "AUTOR" (EMPRESA,EMAIL,NOMBRE,AP_PATERNO,AP_MATERNO,OCUPACION) VALUES ('Iaculis Incorporated','elit@consequatnecmollis.ca','Porter','Carter','William','683145 9828');
 
+
+INSERT INTO "EMPLEADO" (ES_EDITOR,ES_REVISOR,NOMBRE,AP_PATERNO,AP_MATERNO,EMAIL,FECHA_INGRESO) VALUES (1,0,'Omar','Dustin','Nelle','sapien.Aenean@egestas.net','03/11/2018');
+INSERT INTO "EMPLEADO" (ES_EDITOR,ES_REVISOR,NOMBRE,AP_PATERNO,AP_MATERNO,EMAIL,FECHA_INGRESO) VALUES (0,0,'Barry','Hedley','Fay','eleifend@quam.org','03/01/2018');
+INSERT INTO "EMPLEADO" (ES_EDITOR,ES_REVISOR,NOMBRE,AP_PATERNO,AP_MATERNO,EMAIL,FECHA_INGRESO) VALUES (0,0,'Cora','Knox','Camille','convallis.erat.eget@Donec.co.uk','04/28/2018');
+INSERT INTO "EMPLEADO" (ES_EDITOR,ES_REVISOR,NOMBRE,AP_PATERNO,AP_MATERNO,EMAIL,FECHA_INGRESO) VALUES (0,1,'Acton','Zephania','Jerome','lectus.rutrum.urna@parturient.net','11/07/2018');
+INSERT INTO "EMPLEADO" (ES_EDITOR,ES_REVISOR,NOMBRE,AP_PATERNO,AP_MATERNO,EMAIL,FECHA_INGRESO) VALUES (0,1,'Iris','Chaney','Geraldine','est.mauris@ridiculus.net','08/07/2017');
+INSERT INTO "EMPLEADO" (ES_EDITOR,ES_REVISOR,NOMBRE,AP_PATERNO,AP_MATERNO,EMAIL,FECHA_INGRESO) VALUES (1,1,'Driscoll','Holmes','Fiona','ut.dolor.dapibus@nasceturridiculusmus.co.uk','07/03/2018');
+INSERT INTO "EMPLEADO" (ES_EDITOR,ES_REVISOR,NOMBRE,AP_PATERNO,AP_MATERNO,EMAIL,FECHA_INGRESO) VALUES (1,1,'Morgan','Dylan','Gretchen','ut.lacus.Nulla@enimMauris.ca','10/17/2018');
