a954e11e91211bb8b2d3e70f55a2543305558c7a
Gerardo LSJ <gerardolsj@gmail.com>
2018-06-09 20:22:28 -0500

    external table

---
+++  s-04-tablas-externas.sql
@@ -0,0 +1,27 @@
+CREATE DIRECTORY ext_tab_dir AS '/home/oracle/app';
+GRANT READ ON DIRECTORY ext_tab_dir TO prueba1;
+
+
+CREATE TABLE emp_load
+      (employee_number      CHAR(5),
+       employee_dob         CHAR(20),
+       employee_last_name   CHAR(20),
+       employee_first_name  CHAR(15),
+       employee_middle_name CHAR(15),
+       employee_hire_date   DATE)
+    ORGANIZATION EXTERNAL
+      (TYPE ORACLE_LOADER
+      DEFAULT DIRECTORY def_dir1
+      ACCESS PARAMETERS
+        (RECORDS DELIMITED BY NEWLINE
+         FIELDS (employee_number      CHAR(2),
+                 employee_dob         CHAR(20),
+                 employee_last_name   CHAR(18),
+                 employee_first_name  CHAR(11),
+                 employee_middle_name CHAR(11),
+                 employee_hire_date   CHAR(10) date_format DATE mask "mm/dd/yyyy"
+                )
+        )
+      LOCATION ('info.dat')
+     );
+     --Id, Name, Description, StartDate "24-03-2008"
