08ea3d2ea1b8a85c02fecaf3a503351dbde78b18
Gerardo LSJ <gerardolsj@gmail.com>
2018-06-09 21:36:03 -0500

    external 2

--- s-04-tablas-externas.sql
+++ s-04-tablas-externas.sql
@@ -24,4 +24,30 @@ CREATE TABLE emp_load
         )
       LOCATION ('info.dat')
      );
+     
+CREATE TABLE ext_tab (
+empno  CHAR(4),
+ename  CHAR(20),
+job    CHAR(20),
+deptno CHAR(2))
+ORGANIZATION EXTERNAL (
+  TYPE oracle_loader
+  DEFAULT DIRECTORY ext_tab_dir
+    ACCESS PARAMETERS (
+    RECORDS DELIMITED BY NEWLINE
+    BADFILE 'bad_%a_%p.bad'
+    LOGFILE 'log_%a_%p.log'
+    FIELDS TERMINATED BY ','
+    MISSING FIELD VALUES ARE NULL
+    REJECT ROWS WITH ALL NULL FIELDS
+    (
+      empno, 
+      ename, 
+      job, 
+      deptno))
+    LOCATION ('info.dat')
+  )
+PARALLEL
+REJECT LIMIT 0
+NOMONITORING;
      --Id, Name, Description, StartDate "24-03-2008"
