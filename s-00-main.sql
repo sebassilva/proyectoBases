c03af253fcd3f80582a03a26f6c3cdc32c836fdd
Sebastian Silva <seebassilva@gmail.com>
2018-06-11 18:27:25 -0500

    added new procedure

---
+++ s-00-main.sql
@@ -0,0 +1,7 @@
+-- @Autores: López Santibáñez Jiménez, Luis Gerardo
+--          Silva García, Carlos Sebastián
+-- Fecha de creación: 10/06/2018
+-- Descripción: Archivo inicial, llama a todos los demás ejecutar con plsql
+
+
+connect system;
---
+++ s-13-p-calcula-promedio-prueba.sql
@@ -0,0 +1,8 @@
+-- @Autores: López Santibáñez Jiménez, Luis Gerardo
+--          Silva García, Carlos Sebastián
+-- Fecha de creación: 10/06/2018
+-- Descripción: Calcula el promedio de un artículo
+
+
+SELECT * FROM ARTICULO_REVISOR
+CALCULA_PROMEDIO(1);
