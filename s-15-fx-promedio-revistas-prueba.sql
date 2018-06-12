94ad61b1b0ba10cc020d511d7f56b9a43019d387
Sebastian Silva <seebassilva@gmail.com>
2018-06-11 23:47:45 -0500

    added function

---
+++ s-15-fx-promedio-revistas-prueba.sql
@@ -0,0 +1,6 @@
+-- @Autores: López Santibáñez Jiménez, Luis Gerardo
+--          Silva García, Carlos Sebastián
+-- Fecha de creación: 09/06/2018
+-- Descripción: Ejecuta la funci{on que calcula el promedio de los articulos en las revistas
+
+select PROMEDIO_REVISTA(1) as promedio_revista from dual ;
